adf27d3950ee2da4bd970c4431cd46b43ebd4f77
Lukas Kudlacek <90826838+zmizung@users.noreply.github.com>
2022-02-27 21:25:50 +0100

    Add files via upload

---
+++ FightEngine/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Lecture19Composition
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random random = new Random();
+
+			Character goblin = new Character(new AI(random), 15, 6, 4, "random");
+			Character hero = new Character();
+			hero.CreateCharacter();
+
+			Game game = new Game(goblin, hero, new Die(random, 6));
+			game.Run(Console.Out);
+			Console.WriteLine();
+
+			Console.WriteLine("Press any key to quit...");
+			Console.ReadKey();
+		}
+	}
+}
